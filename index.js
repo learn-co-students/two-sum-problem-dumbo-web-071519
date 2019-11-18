bb1a996085b004c2b30d94c353565936fc3e4442
github cjl248 <github cjl248@cornell.edu>
2019-11-18 13:42:26 -0500

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ let sum = 6
 let array = [5, -2, 4, 9, 1]
 
 function bruteForceTwoSum(sum, array){
+  let answer = []
+
   for (let i=0; i <= array.length-1; i++) {
-    let combined = array[i] + array[i+1]
-    if (combined === sum ) {
-      console.log(array[i], array[i+1])
+    let maxIndex = array.length-1
+
+    for (let j=maxIndex; j>=0; j--) {
+
+      if (i !== j) {
+        let sum = array[i] + array[j]
+        let pair = [array[i], array[j]]
+        if (sum === 6) {
+          answer.push(pair)
+          console.log("answer: ", answer)
+        }
+      }
     }
   }
 }
